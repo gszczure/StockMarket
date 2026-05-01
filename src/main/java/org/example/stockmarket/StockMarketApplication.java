7a6b7146883a558ef81be415c9c430a6512ebf53
gszczure <grzegorz.sz1202@gmail.com>
2026-05-01 10:42:58 +0200

    chore: initialize Spring Boot project with basic dependencies

---
+++ src/main/java/org/example/stockmarket/StockMarketApplication.java
@@ -0,0 +1,11 @@
+package org.example.stockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketApplication.class, args);
+    }
+}
